ee5d8c8b4c34965712211ecbaff53aca1a9d633e
KseniySS <kseniysms@gmail.com>
2022-02-15 16:48:22 +0300

    Нужна помощь

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,15 +33,15 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, title, count, sum) {
-    let data = {client: name + phone, order: {address, sum}, goods: [{title, count}]};
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {client: name + phone, order: {address, sum}, goods: [({title, count})] };
 
     let countOfGoods = data.goods.length;
 
     for (let i = 0; i < countOfGoods; i ++) {
-        data.goods.push(data.goods[i]);
+        data.goods.push({title: goods[i].title, count: goods[i].count});
      }
-     data.order.address = address.toString; 
+     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`; 
      data.order.sum = sum;
 
     data.client = name + " " + phone;
